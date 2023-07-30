73dd9cdc5a426f53ee87f29400ab68c287a23764
Madi <madi_gaziz@mail.ru>
2023-07-30 20:29:02 +0600

    Сделал отображение пользователей в админ панели

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from accounts.models import User
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id','first_name','last_name', 'username', 'date_joined', 'last_login', 'is_superuser']
+    list_filter = ['id', 'first_name', 'last_name', 'username', 'password']
+
+
+admin.site.register(User, UserAdmin)
