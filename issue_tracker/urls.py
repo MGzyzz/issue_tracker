3e5a824355e5bea6bf099dc22daae16e4c917ff9
Madi <madi_gaziz@mail.ru>
2023-07-30 22:43:51 +0600

    Создал новые пути для редактирования и удаление проекта

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from issue_tracker import views
 
 urlpatterns = [
+    path('', views.Home.as_view(), name='home'),
     path('detail/<int:id>', views.Detail.as_view(), name='detail'),
     path('project/<int:id>/add', views.Add.as_view(), name='add'),
     path('edit/<int:id>', views.Edit.as_view(), name='edit'),
@@ -9,6 +10,8 @@ urlpatterns = [
     path('project/', views.HomeProject.as_view(), name='home_project'),
     path('project/<int:id>', views.DetailProject.as_view(), name='detail_project'),
     path('project/add/', views.AddProject.as_view(), name='add_project'),
+    path('project/<int:id>/edit', views.EditProject.as_view(), name='edit-project'),
+    path('project/<int:id>/delete', views.DeleteProject.as_view(), name='delete-project'),
     path('project/<int:id>/detail', views.ListUserInProject.as_view(), name='list-user-project'),
     path('project/<int:id>/add_user', views.AddUserInProject.as_view(), name='add-user-project'),
     path('project/<int:id>/delete_user', views.DeleteUserProject.as_view(), name='delete-user-project'),
