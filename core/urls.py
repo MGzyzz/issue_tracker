4a85eeb951891848e4c07ec10250f79067f21e8e
Madi <madi_gaziz@mail.ru>
2023-07-30 22:40:21 +0600

    Сделал небольшие изменения

--- core/urls.py
+++ core/urls.py
@@ -18,11 +18,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from issue_tracker import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('issue_tracker.urls')),
     path('auth/', include('accounts.urls')),
-    path('', views.Home.as_view(), name='home'),
-    path('', include('issue_tracker.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
