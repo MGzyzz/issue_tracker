535d545a5d876bb19ca0bcbdc7e032a935acd50a
Madi <madi_gaziz@mail.ru>
2023-09-20 20:06:16 +0600

    Создал классы TaskDetailView, TaskUpdateView, TaskDeleteView, для выполнение просмотра, изменение и удаление задач

---
+++ api/views.py
@@ -0,0 +1,40 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+from issue_tracker.models import Task
+from .serializers import TaskDetailSerializers, TaskUpdateSerializers
+
+# Create your views here.
+
+
+class TaskDetailView(APIView):
+    def get(self, request, id):
+        try:
+            task = Task.objects.get(id=id)
+            serializer = TaskDetailSerializers(task)
+            return JsonResponse(serializer.data, status=200)
+        except Task.DoesNotExist:
+            return JsonResponse({'error': 'Task not found'}, status=404)
+
+class TaskUpdateView(APIView):
+    def put(self, request, id):
+        try:
+            task = Task.objects.get(id=id)
+        except Task.DoesNotExist:
+            return JsonResponse({'success': 'Not Work'}, status=404)
+        serializer = TaskUpdateSerializers(task, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+
+class TaskDeleteView(APIView):
+    def delete(self, request, id):
+        try:
+            task = Task.objects.get(id=id)
+            task.delete()
+            return JsonResponse({'message': 'Task deleted successfully'}, status=204)
+        except Task.DoesNotExist:
+            return JsonResponse({'error': 'Task not found'}, status=404)
